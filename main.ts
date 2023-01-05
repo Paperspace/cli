75db3f40337ec320570cd32a8270c2220a896af5
Jared Lunde <jlunde@paperspace.com>
2023-01-05 15:02:20 -0500

    chore: set up graphql workflow

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ import {
   Command,
   CompletionsCommand,
 } from "https://deno.land/x/cliffy@v0.25.6/command/mod.ts";
+import { GraphQLClient } from "https://esm.sh/graphql-request@5.1.0";
+import { TypedDocumentNode } from "https://esm.sh/v102/@graphql-typed-document-node/core@3.1.1";
+// import { ActivityLogDocument } from "./paperspace-graphql.ts";
 
 const error = colors.bold.red;
 const warn = colors.bold.yellow;
@@ -12,7 +15,24 @@ const success = colors.bold.green;
 
 const __VERSION__ = "0.0.1";
 
+const endpoint = "https://api.paperspace.com/graphql";
+const gqlClient = new GraphQLClient(endpoint, {
+  headers: {
+    authorization: "Bearer MY_TOKEN",
+  },
+});
+
+function gqlFetch<TData = any, TVariables = Record<string, any>>(
+  operation: TypedDocumentNode<TData, TVariables>,
+  variables?: TVariables
+): Promise<TData> {
+  return gqlClient.request(operation, variables);
+}
+
 if (import.meta.main) {
+  // const response = await gqlFetch(ActivityLogDocument);
+  // response.projectActivities.nodes;
+
   const url = z.string().url();
   /**
    * @see https://cliffy.io/docs@v0.25.0/command/commands
