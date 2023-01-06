40306de361bbd1169cb0a0705d1fd3cdef0e2e4a
Jared Lunde <98760247+jared-paperspace@users.noreply.github.com>
2023-01-06 10:47:42 -0700

    feat: test release (#5)

--- lib/deployments/get.ts
+++ lib/deployments/get.ts
@@ -1,8 +1,8 @@
 import { info } from "../ansi.ts";
 
 export async function get(url: string) {
-  console.log(info("GET"), url);
+  console.log(info("[GET]"), url);
   const result = await fetch(url);
-  console.log(info("Status"), `${result.status} ${result.statusText}`);
+  console.log(info("[Status]"), `${result.status} ${result.statusText}`);
   return await result.json();
 }
