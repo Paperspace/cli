09242032e5a0a552542e10b0792512e00469071c
Jared Lunde <jlunde@paperspace.com>
2023-01-09 12:59:32 -0500

    add debug logs to gql request

--- lib/client.ts
+++ lib/client.ts
@@ -2,6 +2,7 @@ import { GraphQLClient } from "https://deno.land/x/graphql_request@v4.1.0/mod.ts
 import { TypedDocumentNode } from "https://esm.sh/v102/@graphql-typed-document-node/core@3.1.1";
 import { env } from "./env.ts";
 import { formattedVersion } from "./version.ts";
+import { logger } from "./logger.ts";
 
 const gqlClient = new GraphQLClient(env.get("PAPERSPACE_API_URL"), {
   headers: env.get("PAPERSPACE_API_KEY")
@@ -23,7 +24,8 @@ export function gqlFetch<TData = unknown, TVariables = Record<string, unknown>>(
   },
 ): Promise<TData> {
   const { apiUrl, apiKey, headers } = options || {};
-  gqlClient.setEndpoint(apiUrl ?? env.get("PAPERSPACE_API_URL"));
+  const endpoint = apiUrl ?? env.get("PAPERSPACE_API_URL");
+  gqlClient.setEndpoint(endpoint);
   gqlClient.setHeader(
     "authorization",
     `Bearer ${apiKey ?? env.get("PAPERSPACE_API_KEY")}`,
@@ -33,5 +35,16 @@ export function gqlFetch<TData = unknown, TVariables = Record<string, unknown>>(
     gqlClient.setHeaders(headers);
   }
 
+  logger.debug(`[GraphQL Request] ${endpoint}`);
+
+  for (const definition of operation.definitions) {
+    if (definition.kind === "OperationDefinition") {
+      logger.debug(
+        `${definition.operation} ${definition.name?.value}(${
+          JSON.stringify(variables, null, 2)
+        })`,
+      );
+    }
+  }
   return gqlClient.request(operation, variables);
 }
