123f3811349c80cb973ab86557958364e4da27c7
Kevin Wang <kwang@paperspace.com>
2023-06-27 13:22:24 -0400

    feat: add requireInGoodStanding meta property

--- commands/machine/update/mod.ts
+++ commands/machine/update/mod.ts
@@ -79,6 +79,7 @@ export const update = command("update", {
   // set. If not, we'll throw an error.
   meta: {
     requireApiKey: true,
+    requireInGoodStanding: true,
   },
 }).run(
   async function* ({ args, flags }) {
--- commands/public-ip/claim/mod.ts
+++ commands/public-ip/claim/mod.ts
@@ -32,6 +32,7 @@ export const claim = command("claim", {
   // set. If not, we'll throw an error.
   meta: {
     requireApiKey: true,
+    requireInGoodStanding: true,
   },
 }).run(
   async function* ({ flags }) {
