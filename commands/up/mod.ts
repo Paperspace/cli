3a82ffcd30d66929d46c122068ce79c939597a2f
ps-kwang <135043922+ps-kwang@users.noreply.github.com>
2023-06-27 15:21:18 -0400

    Update commands/up/mod.ts
    Co-authored-by: Jared Lunde <98760247+jared-paperspace@users.noreply.github.com>

--- commands/up/mod.ts
+++ commands/up/mod.ts
@@ -180,7 +180,7 @@ export async function* runUp(
       yield `✨ ${fmt.colors.bold("Your app is ready")}\n`;
       yield "   " + fmt.colors.bold("Console URL");
       yield "   " + new URL(
-        `/${team}/projects/${project.id}/apps/${deployment.id}`,
+        `/${team}/apps/${deployment.id}`,
         env.get("PAPERSPACE_CONSOLE_URL"),
       ).toString();
       yield "";
