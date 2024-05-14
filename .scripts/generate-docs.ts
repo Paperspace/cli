008c03b834cc7b2aff2c28596a0f92884fa7fe1c
Kevin Wang <kwang@digitalocean.com>
2024-05-14 05:04:55 +0000

    feat: add markdown docs generator and update generated docs

--- .scripts/generate-docs.ts
+++ .scripts/generate-docs.ts
@@ -1,6 +1,9 @@
 import { app } from "../zcli.ts";
 import { root } from "../commands/mod.ts";
-import { zcliJson } from "https://deno.land/x/zcli@1.3.3/zcli-json.ts";
+import {
+  zcliJson,
+  ZcliJsonCommand,
+} from "https://deno.land/x/zcli@1.3.3/zcli-json.ts";
 
 // Read the markdown file adjacent to this script
 const json = await zcliJson(app, root);
@@ -10,3 +13,40 @@ await Deno.writeTextFile(
   ".assets/pspace.json",
   JSON.stringify(json, null, 2),
 );
+
+// Also generate markdown for the CLI
+let markdown = "";
+markdown += "# Paperspace CLI\n\n";
+markdown += json.info.description + "\n\n";
+
+function renderCommand(path: string[], command: ZcliJsonCommand) {
+  markdown += `## ${path.join(" ")}\n\n`;
+  markdown += command.description + "\n\n";
+  markdown += "### Usage\n\n";
+  markdown += `\`\`\`\n`;
+  markdown += `${command.usage}\n`;
+  markdown += "\`\`\`\n\n";
+  markdown += "### Flags\n\n";
+  markdown += "| Name | Aliases | Description | Required |\n";
+  markdown += "| --- | --- | --- | --- |\n";
+  for (const flag of command.flags) {
+    markdown += `| ${flag.name} | ${
+      flag.aliases.join(", ")
+    } | ${flag.description} | ${flag.required} |\n`;
+  }
+  markdown += "\n";
+
+  markdown += "### Subcommands\n\n";
+  for (const subcommand of command.commands) {
+    markdown += `- [${subcommand.name}](#${subcommand.name})\n`;
+  }
+  for (const subcommand of command.commands) {
+    renderCommand([...path, subcommand.name], subcommand);
+  }
+}
+
+for (const command of json.commands) {
+  renderCommand([command.name], command);
+}
+
+await Deno.writeTextFile(".assets/paperspace.md", markdown);
