d8b7c39a4283319e01410a04f39ca41e4e91dadb
Nitin Goyal <ngoyal@digitalocean.com>
2026-02-08 02:48:29 +0530

    structure fix

--- commands/machine/schemas.ts
+++ commands/machine/schemas.ts
@@ -1,14 +1,18 @@
 import { z } from "../../zcli.ts";
 
-export const MachineAutoSnapshotFrequencySchema = z.enum([
-  "hourly",
-  "daily",
-  "weekly",
-  "monthly",
-]).optional();
-
-export const MachineRestorePointFrequencySchema = z.enum(["shutdown"])
+export const MachineAutoSnapshotFrequencySchema = z
+  .enum([
+    "hourly",
+    "daily",
+    "weekly",
+    "monthly",
+  ])
   .optional();
 
-export const MachinePublicIpTypeSchema = z.enum(["static", "dynamic", "none"])
+export const MachineRestorePointFrequencySchema = z
+  .enum(["shutdown"])
+  .optional();
+
+export const MachinePublicIpTypeSchema = z
+  .enum(["static", "dynamic", "none"])
   .optional();
